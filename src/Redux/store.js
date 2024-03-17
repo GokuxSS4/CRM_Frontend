009b0e36c57ee709044ee711d4f519647ebe8371
jaykumar286 <jaykumarchavda2806@gmail.com>
2024-03-17 18:51:41 +0530

    add an basic eslint configs

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import authReducer from "./Slices/AuthSlice";
 const store = configureStore({
     reducer:{
         auth:authReducer
     },
     devTools:true
-})
+});
 
 export default store;
